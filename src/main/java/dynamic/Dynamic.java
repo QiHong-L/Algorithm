8fd7c1ec4a638ec990571fdd935b0f107b19d124
liuqihong <36610439@qq.com>
2022-07-09 10:22:13 +0800

    最长的斐波那契子序列的长度

--- src/main/java/dynamic/Dynamic.java
+++ src/main/java/dynamic/Dynamic.java
@@ -192,4 +192,31 @@ public class Dynamic {
         }
         return baseCount > evenCount ? evenCount : baseCount;
     }
+
+    /**
+     * 873. 最长的斐波那契子序列的长度
+     * @param arr
+     * @return
+     */
+    public int lenLongestFibSubseq(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i],i);
+        }
+        int[][] dp = new int[n][n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j > 0 && arr[j] * 2 > arr[i]; j--) {
+                int k = map.getOrDefault(arr[i] - arr[j],-1);
+                if (k >= 0) {
+                    dp[j][i] = Math.max(dp[k][j] + 1,3);
+                }
+                ans = Math.max(ans,dp[j][i]);
+            }
+        }
+        return ans;
+    }
+
+
 }
