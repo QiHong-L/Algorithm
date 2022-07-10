a4b23647fede80d1db2c38d2e4c1fa39debbbe12
liuqihong <36610439@qq.com>
2022-07-10 10:38:34 +0800

    摘樱桃

--- src/main/java/dynamic/Dynamic.java
+++ src/main/java/dynamic/Dynamic.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 public class Dynamic {
     /**
      * 871. 最低加油次数
+     *
      * @param target
      * @param startFuel
      * @param stations
@@ -23,8 +24,8 @@ public class Dynamic {
         dp[0] = startFuel;
         for (int i = 0; i < n; i++) {
             for (int j = i; j >= 0; j--) {
-                if(dp[j] >= stations[i][0]) {
-                    dp[j+1] = Math.max(dp[j+1],dp[j] + stations[i][1]);
+                if (dp[j] >= stations[i][0]) {
+                    dp[j + 1] = Math.max(dp[j + 1], dp[j] + stations[i][1]);
                 }
             }
         }
@@ -38,13 +39,14 @@ public class Dynamic {
 
     /**
      * 1200. 最小绝对差
+     *
      * @param arr
      * @return
      */
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(arr);
-        int i = 0,j = 1;
+        int i = 0, j = 1;
         int num = Integer.MAX_VALUE;
         while (j < arr.length) {
             int abs = Math.abs(arr[j] - arr[i]);
@@ -64,6 +66,7 @@ public class Dynamic {
 
     /**
      * 736. Lisp 语法解析
+     *
      * @param expression
      * @return
      */
@@ -101,7 +104,7 @@ public class Dynamic {
                 vars.add(var);
                 start++;
                 int e = innerEvaluate(expression);
-                scope.putIfAbsent(var,new ArrayDeque<Integer>());
+                scope.putIfAbsent(var, new ArrayDeque<Integer>());
                 scope.get(var).push(e);
                 start++;
             }
@@ -109,13 +112,13 @@ public class Dynamic {
                 scope.get(var).pop();
             }
         } else if (expression.charAt(start) == 'a') {
-            start+=4;
+            start += 4;
             int e1 = innerEvaluate(expression);
             start++;
             int e2 = innerEvaluate(expression);
             ret = e1 + e2;
         } else {
-            start+=5;
+            start += 5;
             int e1 = innerEvaluate(expression);
             start++;
             int e2 = innerEvaluate(expression);
@@ -152,6 +155,7 @@ public class Dynamic {
 
     /**
      * 648. 单词替换
+     *
      * @param dictionary
      * @param sentence
      * @return
@@ -159,13 +163,13 @@ public class Dynamic {
     public String replaceWords(List<String> dictionary, String sentence) {
         String[] s = sentence.split(" ");
         for (int i = 0; i < s.length; i++) {
-            s[i] = isRoot(dictionary,s[i]);
+            s[i] = isRoot(dictionary, s[i]);
         }
         String collect = Arrays.stream(s).collect(Collectors.joining(" "));
         return collect;
     }
 
-    public String isRoot(List<String> dictionary,String str) {
+    public String isRoot(List<String> dictionary, String str) {
         int size = Integer.MAX_VALUE;
         String result = str;
         for (String root : dictionary) {
@@ -181,11 +185,12 @@ public class Dynamic {
 
     /**
      * 1217.玩筹码
+     *
      * @param position
      * @return
      */
     public int minCostToMoveChips(int[] position) {
-        int baseCount = 0,evenCount = 0;
+        int baseCount = 0, evenCount = 0;
         for (int i = 0; i < position.length; i++) {
             if (position[i] % 2 == 0) evenCount++;
             else baseCount++;
@@ -195,28 +200,73 @@ public class Dynamic {
 
     /**
      * 873. 最长的斐波那契子序列的长度
+     *
      * @param arr
      * @return
      */
     public int lenLongestFibSubseq(int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            map.put(arr[i],i);
+            map.put(arr[i], i);
         }
         int[][] dp = new int[n][n];
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i - 1; j > 0 && arr[j] * 2 > arr[i]; j--) {
-                int k = map.getOrDefault(arr[i] - arr[j],-1);
+                int k = map.getOrDefault(arr[i] - arr[j], -1);
                 if (k >= 0) {
-                    dp[j][i] = Math.max(dp[k][j] + 1,3);
+                    dp[j][i] = Math.max(dp[k][j] + 1, 3);
                 }
-                ans = Math.max(ans,dp[j][i]);
+                ans = Math.max(ans, dp[j][i]);
             }
         }
         return ans;
     }
 
-
+    /**
+     * 741. 摘樱桃
+     * @param grid
+     * @return
+     */
+    public int cherryPickup(int[][] grid) {
+        int n = grid.length;
+        int[][][] f = new int[n * 2 - 1][n][n];
+        for (int i = 0; i < n * 2 - 1; ++i) {
+            for (int j = 0; j < n; ++j) {
+                Arrays.fill(f[i][j], Integer.MIN_VALUE);
+            }
+        }
+        f[0][0][0] = grid[0][0];
+        for (int k = 1; k < n * 2 - 1; ++k) {
+            for (int x1 = Math.max(k - n + 1, 0); x1 <= Math.min(k, n - 1); ++x1) {
+                int y1 = k - x1;
+                if (grid[x1][y1] == -1) {
+                    continue;
+                }
+                for (int x2 = x1; x2 <= Math.min(k, n - 1); ++x2) {
+                    int y2 = k - x2;
+                    if (grid[x2][y2] == -1) {
+                        continue;
+                    }
+                    int res = f[k - 1][x1][x2]; // 都往右
+                    if (x1 > 0) {
+                        res = Math.max(res, f[k - 1][x1 - 1][x2]); // 往下，往右
+                    }
+                    if (x2 > 0) {
+                        res = Math.max(res, f[k - 1][x1][x2 - 1]); // 往右，往下
+                    }
+                    if (x1 > 0 && x2 > 0) {
+                        res = Math.max(res, f[k - 1][x1 - 1][x2 - 1]); // 都往下
+                    }
+                    res += grid[x1][y1];
+                    if (x2 != x1) { // 避免重复摘同一个樱桃
+                        res += grid[x2][y2];
+                    }
+                    f[k][x1][x2] = res;
+                }
+            }
+        }
+        return Math.max(f[n * 2 - 2][n - 1][n - 1], 0);
+    }
 }
