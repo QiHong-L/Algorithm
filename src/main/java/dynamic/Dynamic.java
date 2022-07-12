7ec9a824b4f60ae671d02977c04ebc5abeb53b76
liuqihong <36610439@qq.com>
2022-07-12 11:00:09 +0800

    奇数值单元格的数目

--- src/main/java/dynamic/Dynamic.java
+++ src/main/java/dynamic/Dynamic.java
@@ -226,6 +226,7 @@ public class Dynamic {
 
     /**
      * 741. 摘樱桃
+     *
      * @param grid
      * @return
      */
@@ -268,5 +269,33 @@ public class Dynamic {
             }
         }
         return Math.max(f[n * 2 - 2][n - 1][n - 1], 0);
+    }
+
+    /**
+     * 1252. 奇数值单元格的数目
+     * @param m
+     * @param n
+     * @param indices
+     * @return
+     */
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+        for (int[] index : indices) {
+            rows[index[0]]++;
+            cols[index[1]]++;
+        }
+        int oddx = 0, oddy = 0;
+        for (int i = 0; i < m; i++) {
+            if ((rows[i] & 1) != 0) {
+                oddx++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if ((rows[i] & 1) != 0) {
+                oddy++;
+            }
+        }
+        return oddx * (n - oddy) + (m - oddx) * oddy;
     }
 }
