570fdd22bf78b49793c81f1cec816763d80af50e
liuqihong <36610439@qq.com>
2022-07-16 10:44:41 +0800

    四叉树交集

--- src/main/java/dynamic/Dynamic.java
+++ src/main/java/dynamic/Dynamic.java
@@ -357,6 +357,57 @@ public class Dynamic {
         return ans;
     }
 
+    class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+
+        public Node() {}
+
+        public Node(boolean _val,boolean _isLeaf,Node _topLeft,Node _topRight,Node _bottomLeft,Node _bottomRight) {
+            val = _val;
+            isLeaf = _isLeaf;
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+        }
+    }
+
+    /**
+     * 558. 四叉树交集
+     * @param quadTree1
+     * @param quadTree2
+     * @return
+     */
+    public Node intersect(Node quadTree1, Node quadTree2) {
+        if (quadTree1.isLeaf) {
+            if (quadTree1.val) {
+                return new Node(true,true,null,null,null,null);
+            }
+            return quadTree2;
+        }
+        if (quadTree2.isLeaf) {
+            if (quadTree2.val) {
+                return new Node(true,true,null,null,null,null);
+            }
+            return quadTree1;
+        }
+        Node topLeft = intersect(quadTree1.topLeft,quadTree2.topLeft);
+        Node topRight = intersect(quadTree1.topRight,quadTree2.topRight);
+        Node bottomLeft = intersect(quadTree1.bottomLeft,quadTree2.bottomLeft);
+        Node bottomRight = intersect(quadTree1.bottomRight,quadTree2.bottomRight);
+        //四个子节点都是叶子结点并且值相同
+        if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf
+                && topLeft.val == topRight.val && topLeft.val == bottomLeft.val && topLeft.val == bottomRight.val) {
+            return new Node(topLeft.val, true, null, null, null, null);
+        }
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
     public static void main(String[] args) {
         asteroidCollision(new int[]{-2,-2,1,-2});
     }
