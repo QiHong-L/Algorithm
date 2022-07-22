a599071fb98c24b9852bb925907c0ed218b249a1
liuqihong <36610439@qq.com>
2022-07-22 17:44:09 +0800

    设置交集大小至少为2

--- src/main/java/dynamic/Dynamic.java
+++ src/main/java/dynamic/Dynamic.java
@@ -751,6 +751,46 @@ public class Dynamic {
         return root;
     }
 
+    /**
+     * 设置交集大小至少为2
+     * @param intervals
+     * @return
+     */
+    public int intersectionSizeTwo(int[][] intervals) {
+        int n = intervals.length;
+        int res = 0;
+        int m = 2;
+        Arrays.sort(intervals,(a,b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+        List<Integer>[] temp = new List[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = new ArrayList<>();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = intervals[i][0],k = temp[i].size(); k < m; j++,k++) {
+                res++;
+                help(intervals,temp,i-1,j);
+            }
+        }
+        return res;
+
+    }
+
+    public void help(int[][] intervals, List<Integer>[] temp, int pos, int num) {
+        for (int i = pos; i >= 0; i--) {
+            if (intervals[i][1] < num) {
+                break;
+            }
+            temp[i].add(num);
+        }
+    }
+
+
+
         public static void main(String[] args) {
         asteroidCollision(new int[]{-2,-2,1,-2});
     }
