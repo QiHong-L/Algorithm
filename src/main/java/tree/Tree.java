5501f82792eae1a26fc5ec4d998f1e7c1e7be7f7
liuqihong <36610439@qq.com>
2022-09-02 20:36:22 +0800

    最长同值路径

---
+++ src/main/java/tree/Tree.java
@@ -0,0 +1,34 @@
+package tree;
+
+/**
+ * @Author: liuqihong
+ * @Description: 687. 最长同值路径
+ * @Date Created in  2022-09-02 20:17
+ * @Modified By:
+ */
+public class Tree {
+
+    int res;
+
+    public int longestUnivaluePath(TreeNode root) {
+        res = 0;
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left),right = dfs(root.right);
+        int left1 =0, right1 = 0;
+        if (root.left != null && root.left.val == root.val) {
+            left1 = left + 1;
+        }
+        if (root.right != null && root.right.val == root.val) {
+            right1 = right + 1;
+        }
+        res = Math.max(res,left1 + right1);
+        return Math.max(left1,right1);
+    }
+}
